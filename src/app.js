e26ae33e2aa5f6ac45dc5a31aed10d67f7c013bc
Jean Melo <jeandejavu@hotmail.com>
2020-08-29 10:07:40 -0300

    feat: implementacoes recursos repositories

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { v4: uuid } = require('uuid');
+const { v4: uuid } = require("uuid");
 
 const app = express();
 
@@ -11,23 +11,61 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
-app.post("/repositories", (request, response) => {
-  // TODO
+app.post("/repositories", ({ body: { url, title, techs } }, response) => {
+  const repository = {
+    id: uuid(),
+    url,
+    title,
+    techs,
+    likes: 0,
+  };
+  repositories.push(repository);
+
+  return response.json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put(
+  "/repositories/:id",
+  ({ params: { id }, body: { url, title, techs } }, response) => {
+    const index = repositories.findIndex((repository) => repository.id === id);
+    if (index > -1) {
+      repositories[index] = {
+        ...repositories[index],
+        url,
+        title,
+        techs,
+      };
+      return response.json(repositories[index]);
+    }
+
+    return response.status(400).send();
+  }
+);
+
+app.delete("/repositories/:id", ({ params: { id } }, response) => {
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index > -1) {
+    repositories.splice(index, 1);
+    return response.status(204).send();
+  }
+
+  return response.status(400).send();
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
-});
+app.post("/repositories/:id/like", ({ params: { id } }, response) => {
+  const index = repositories.findIndex((repository) => repository.id === id);
+  if (index > -1) {
+    repositories[index] = {
+      ...repositories[index],
+      likes: repositories[index].likes + 1,
+    };
+    return response.json(repositories[index]);
+  }
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  return response.status(400).send();
 });
 
 module.exports = app;
